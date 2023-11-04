2d14cf81eb53ee198cdc7caa7798aa64a1b6e760
joshua <gonzalesjoshdre@gmail.com>
2023-11-04 21:43:17 +0800

    commit docker and sequelize library files

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-import express from "express";
+const express = require("express");
 
-const app = express();
+// npm library for generating random data
+const { Sequelize } = require("sequelize");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// pass in postgres environment variable
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const app = express();
+app.use(express.json());
+
+const PORT = 8080;
+
+const dataList = [];
+
+app.get("/data", (req, res) => {
+  res.status(200).send(dataList);
+  return;
+});
+
+app.post("/data", (req, res) => {
+  let data = req.body;
+  dataList.push(data);
+  res.status(201).send(data);
+  return;
+});
+
+app.listen(PORT, () => {
+  try {
+    sequelize.authenticate();
+    console.log("Connection successful to the database");
+  } catch (error) {
+    console.log("Connection unsuccessful to the database", error);
+  }
+  console.log(`Server is listening on port ${PORT}`);
 });
